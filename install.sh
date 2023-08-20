19d220bb200d792f4d119e911c521d4988ecc3dc
lcs <lucasan123sky@gmail.com>
2023-08-20 10:24:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 
 sleep 2
+
 /usr/bin/g++ -Wall -march=native -Ofast -funroll-loops -pthread Qiner70AVX512.cpp -o Qiner70AVX512
-/usr/bin/g++ -Wall -march=native -Ofast -funroll-loops -pthread qiner-70_linux.cpp -o qiner-70_linux
-/usr/bin/g++ -Wall -march=native -Ofast -funroll-loops -pthread qiner-70.2_linux.cpp -o qiner-70.2_linux
-cp Qiner70AVX512 Qiner70 qiner-70_linux qiner-70.2_linux ..
+
+echo avx512 finished
+
+/usr/bin/g++ -Wall -march=native -Ofast -funroll-loops -pthread qiner-70.3-turbo_linux.cpp -o qiner-70_linux
+
+
+cp Qiner70AVX512  qiner-70_linux  ..
 cd
